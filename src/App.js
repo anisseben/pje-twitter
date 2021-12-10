47cf533c1557fbd1410fe52be8869e20c879387e
anisse <benabdallah>
2021-12-10 17:24:21 +0100

    daploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "./Components/Home"
 
 import axios from 'axios';
-axios.defaults.baseURL = "http://127.0.0.1:5000/";
+axios.defaults.baseURL = "https://pje-twitter-api.herokuapp.com/";
 
 
 function App() {
